4b96c5f7c6ef455494436ba28ce3579308204a94
Sebastian Loguzzo <sebastian.loguzzo@gmail.com>
2020-11-05 09:28:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import bas from "./assets/img/bas.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <img src={bas} alt="en construccion" style={{width:'100%'}}/>;
 }
 
 export default App;
